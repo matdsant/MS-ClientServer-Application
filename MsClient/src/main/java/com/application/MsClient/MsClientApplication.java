05962c7e5e9529efa6c1189c51fc67dd8f6f9e5e
Matheus dos Santos <matheus.dso1996@gmail.com>
2024-07-13 21:21:04 -0300

    New configurations in the "Application.yml"

--- MsClient/src/main/java/com/application/MsClient/MsClientApplication.java
+++ MsClient/src/main/java/com/application/MsClient/MsClientApplication.java
@@ -2,8 +2,10 @@ package com.application.MsClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsClientApplication {
 
 	public static void main(String[] args) {
